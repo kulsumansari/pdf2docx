2a2603b9681a027cebf6d649604ce6471574cd01
kulsumansari <kulsumansari4@gmail.com>
2024-05-24 17:31:59 +0530

    changes

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-from flask import Flask
-# import os
+from flask import Flask, request
+import os
 # import json
-# from utility import download_pdf, convertPdf2Docx, upload2CMS
+from utility import download_pdf, convertPdf2Docx
 
 app = Flask(__name__)
 
@@ -9,37 +9,38 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-# @app.route('/convert', methods=['POST'])
-# def convert():
-#     asset = request.json
+@app.route('/convert', methods=['POST'])
+def convert():
+    asset = request.json
+    print(request)
 
-#     pdf_url = asset['url']
-#     print(pdf_url)
+    pdf_url = asset['url']
+    print(pdf_url)
     
-#     local_pdf_path = 'temp.pdf'
-#     local_docx_path = 'temp.docx'
+    local_pdf_path = '/tmp/temp.pdf'
+    local_docx_path = '/tmp/temp.docx'
 
-#     try:
-#         # Step 1: Download PDF from Cloudinary
-#         download_pdf(pdf_url, local_pdf_path)
+    try:
+        # Step 1: Download PDF from Cloudinary
+        download_pdf(pdf_url, local_pdf_path)
 
-#         # Step 2: Convert PDF to DOCX
-#         convertPdf2Docx(local_pdf_path, local_docx_path)
+        # Step 2: Convert PDF to DOCX
+        convertPdf2Docx(local_pdf_path, local_docx_path)
 
-#         # Step 3: Upload DOCX to Cloudinary
-#         response = upload2CMS(local_docx_path)
+        # Step 3: Upload DOCX to Cloudinary
+        # response = upload2CMS(local_docx_path)
         
-#         # if response.status_code == 201:
-#         #     print(f'Process completed successfully:\n {response.json()}')
+        # if response.status_code == 201:
+        #     print(f'Process completed successfully:\n {response.json()}')
 
-#     finally:
-#         # Clean up local files
-#         if os.path.exists(local_pdf_path):
-#             os.remove(local_pdf_path)
-#         if os.path.exists(local_docx_path):
-#             os.remove(local_docx_path)
+    finally:
+        # Clean up local files
+        if os.path.exists(local_pdf_path):
+            os.remove(local_pdf_path)
+        if os.path.exists(local_docx_path):
+            os.remove(local_docx_path)
 
-#     return {"messge": "done"}, 200
+    return {"messge": "done"}, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utility.py
+++ utility.py
@@ -1,52 +1,52 @@
-# import requests
-# from pdf2docx import Converter
+import requests
+from pdf2docx import Converter
 # from dotenv import dotenv_values
 
 # config = dotenv_values(".env")
 
 
-# def download_pdf(pdf_url, local_pdf_path):
+def download_pdf(pdf_url, local_pdf_path):
     
-#     response = requests.get(pdf_url)
-#     with open(local_pdf_path, 'wb') as file:
-#         file.write(response.content)
-#     print(f"Downloaded PDF from {pdf_url} to {local_pdf_path}")
+    response = requests.get(pdf_url)
+    with open(local_pdf_path, 'wb') as file:
+        file.write(response.content)
+    print(f"Downloaded PDF from {pdf_url} to {local_pdf_path}")
     
 
-# # convert to local docx
-# def convertPdf2Docx(local_pdf_path, local_docx_path):
+# convert to local docx
+def convertPdf2Docx(local_pdf_path, local_docx_path):
 
-#     cv = Converter(local_pdf_path)
-#     cv.convert(local_docx_path)
-#     cv.close()
-#     print(f"Converted PDF from {local_pdf_path} to DOCX at {local_docx_path}")
+    cv = Converter(local_pdf_path)
+    cv.convert(local_docx_path)
+    cv.close()
+    print(f"Converted PDF from {local_pdf_path} to DOCX at {local_docx_path}")
     
     
-# # upload to CMS
+# upload to CMS
 # def upload2CMS(local_docx_path):
-#     import os
+    import os
 
-#     try:
-#         url = "https://api.contentstack.io/v3/assets"
+    try:
+        url = "https://api.contentstack.io/v3/assets"
 
-#         payload = {'asset[title]': 'doc2222'}
-#         files=[
-#             ('asset[upload]',('sample1.docx',open(local_docx_path,'rb'),'application/octet-stream'))
-#         ]
-#         headers = {
-#             'api_key': config['API_KEY'],
-#             'authorization': config['AUTHORIZATION']
-#         }
+        payload = {'asset[title]': 'doc2222'}
+        files=[
+            ('asset[upload]',('sample1.docx',open(local_docx_path,'rb'),'application/octet-stream'))
+        ]
+        headers = {
+            'api_key': config['API_KEY'],
+            'authorization': config['AUTHORIZATION']
+        }
 
-#         response = requests.request('POST', url, headers=headers, data=payload, files=files)
-#         print("🚀 ~ response:", response.text)
+        response = requests.request('POST', url, headers=headers, data=payload, files=files)
+        print("🚀 ~ response:", response.text)
 
-#         # print(response)
-#         # if response.status != 201:
-#         #     raise Exception(response.text)
+        # print(response)
+        # if response.status != 201:
+        #     raise Exception(response.text)
         
-#         return response
+        return response
 
-#     except Exception as error:
-#         print(f"Error while uploading document: {error}")
-#         return error
+    except Exception as error:
+        print(f"Error while uploading document: {error}")
+        return error
