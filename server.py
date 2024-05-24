a229529448a4ccc63be327a0fde2871714cdbb7a
kulsumansari <kulsumansari4@gmail.com>
2024-05-24 16:54:34 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import os
+import json
+from utility import download_pdf, convertPdf2Docx, upload2CMS
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    asset = request.json
+
+    pdf_url = asset['url']
+    print(pdf_url)
+    
+    local_pdf_path = 'temp.pdf'
+    local_docx_path = 'temp.docx'
+
+    try:
+        # Step 1: Download PDF from Cloudinary
+        download_pdf(pdf_url, local_pdf_path)
+
+        # Step 2: Convert PDF to DOCX
+        convertPdf2Docx(local_pdf_path, local_docx_path)
+
+        # Step 3: Upload DOCX to Cloudinary
+        response = upload2CMS(local_docx_path)
+        
+        # if response.status_code == 201:
+        #     print(f'Process completed successfully:\n {response.json()}')
+
+    finally:
+        # Clean up local files
+        if os.path.exists(local_pdf_path):
+            os.remove(local_pdf_path)
+        if os.path.exists(local_docx_path):
+            os.remove(local_docx_path)
+
+    return {"messge": "done"}, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
